5535aa1cb13c7d80fb72410439de507b3b360ed1
ahamedsiammia <md7559263@gmail.com>
2026-09-23 16:37:38 +0600

    change local app link

--- src/server.ts
+++ src/server.ts
@@ -5,25 +5,27 @@ import { prisma } from "./lib/prisma";
 import { redisClient } from "./lib/redis";
 
 
-async function main() {
-    try {
-        await prisma.$connect();
-        console.log("Connected to The Database Successfully");
-        await redisClient.connect();
-		console.log("Redis connect Successfully");
+// async function main() {
+//     try {
+//         await prisma.$connect();
+//         console.log("Connected to The Database Successfully");
+//         await redisClient.connect();
+// 		console.log("Redis connect Successfully");
 
-		await transporter.verify();
-		console.log("NodeMailer Connected Successfully");
+// 		await transporter.verify();
+// 		console.log("NodeMailer Connected Successfully");
 
         
-    app.listen(config.port,()=>{
-        console.log(`server on running port ${config.port}`);
-    })
-    } catch (error) {
-        console.log("Error starting the server",error);
-        await prisma.$disconnect();
-        process.exit(1);
-    }
-};
+//     app.listen(config.port,()=>{
+//         console.log(`server on running port ${config.port}`);
+//     })
+//     } catch (error) {
+//         console.log("Error starting the server",error);
+//         await prisma.$disconnect();
+//         process.exit(1);
+//     }
+// };
 
-main();
+// main();
+
+export default app
